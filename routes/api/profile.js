9af22f2b1f135063c03f2bb2e84e67ad5887401c
mohammedpythons <mohammedshokai99@gmail.com>
2021-10-02 18:50:47 -0400

    create delete user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -132,4 +132,25 @@ router.get(`/user/:user_id`, async (req, res) => {
   }
 });
 
+//@route delete api/profile
+//@description delete profile, user and posts
+// @access private
+router.delete("/", auth ,async (req, res) => {
+    try {
+        //@todo - remove posts
+
+
+        // Remove profile
+      await Profile.findOneAndRemove({ user: req.user.id })
+      // Remove user
+      await User.findOneAndRemove({ _id: req.user.id })
+
+
+      res.json({msg: "User deleted "});
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  });
+
 module.exports = router;
