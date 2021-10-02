f658cb4efd8014d3e3099dd9133a5af463d48ab0
mohammedpythons <mohammedshokai99@gmail.com>
2021-10-02 18:13:48 -0400

    add user/:user_id route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -99,8 +99,37 @@ router.post(
   }
 );
 
+//@route get api/profile
+//@description get all profile
+// @access public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
-
-
+//@route get api/profile/user/:user_id
+//@description get profile by user id
+// @access public
+router.get(`/user/:user_id`, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+    if (!profile)
+      return res.status(400).json({ msg: "profile not found" });
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+        return res.status(400).json({ msg: "profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
