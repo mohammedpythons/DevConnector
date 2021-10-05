c0eace62a1d04f88d72fac912e7cab70951b2a75
mohammedpythons <mohammedshokai99@gmail.com>
2021-10-05 13:37:53 -0400

    create post model

--- models/User.js
+++ models/User.js
@@ -2,26 +2,26 @@ const mongoose = require("mongoose");
 
 // set a schema for the users with mongoose
 const UserSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:{
-        type: String,
-        required: true,
-    },
-    avatar: {
-        type: String,
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = User = mongoose.model("user", UserSchema);
