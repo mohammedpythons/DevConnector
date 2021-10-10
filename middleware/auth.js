74e87dbf2d82f0ec23855df1c6a70d1404c27e48
mohammedpythons <mohammedshokai99@gmail.com>
2021-10-10 18:15:15 -0400

    set up client side with react

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,27 +1,22 @@
-const jwt = require("jsonwebtoken")
-const config = require("config")
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
+module.exports = function (req, res, next) {
+  // Get the token form the header
+  const token = req.header("x-auth-token");
 
+  //check if not token
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
 
-module.exports =  function(req, res, next) {
-    // Get the token form the header
-    const token = req.header("x-auth-token");
+  // Veryfiy token
+  try {
+    const decodded = jwt.verify(token, config.get("jwtSecret"));
 
-
-    //check if not token
-    if (!token) {
-        return res.status(401).json({msg: "No token, authorization denied"});
-    }
-
-
-    // Veryfiy token
-    try {
-        const decodded = jwt.verify(token, config.get("jwtSecret"));
-
-        req.user = decodded.user;
-        next();
-    } catch(err) {
-        res.status(401).json({msg: "Token is not valid"})
-    }
-
-}
+    req.user = decodded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
