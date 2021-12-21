55045382f994b937d116c99fcaf450d86d509b6a
mohammedpythons <mohammedshokai99@gmail.com>
2021-12-21 14:58:39 -0500

    prepare to deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const cors = require('cors');
+const cors = require("cors");
+const path = require("path");
 const app = express();
 
 // connect Database
@@ -10,15 +11,22 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("API is running!!");
-});
-
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/post"));
 app.use("/api/profile", require("./routes/api/profile"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set Static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
